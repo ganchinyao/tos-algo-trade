385d1dec803ec1dfc1c9398b0174e8827d46f172
Gan Chin Yao <chinyao95@gmail.com>
2022-09-04 03:31:35 +0800

    Add endpoint to market close all currently open orders

--- src/index.ts
+++ src/index.ts
@@ -88,6 +88,32 @@ app.post("/market_sell", async (req: Request, res: Response) => {
 });
 
 /**
+ * Send a POST request to close all existing open orders, if any, for all strategies.
+ * If there is no open orders, do nothing.
+ *
+ * E.g curl -X POST -H "Content-Type: application/json" -d '{"auth": "MY_SOME_AUTH"}' http://localhost:8000/market_close_all
+ *
+ * Request signature:
+ *    body: {
+ *      "auth": string
+ *    }
+ *
+ * Response signature:
+ *    Succeeds: 200 "Closed all open orders successfully."
+ *    Error: 503 "Server error"
+ *
+ */
+app.post("/market_close_all", async (req: Request, res: Response) => {
+  try {
+    await Order.marketCloseAllOpenOrders();
+    res.send("Closed all open orders successfully.");
+  } catch (err: any) {
+    addErrorToLogbook(Date.now(), err.toString());
+    return res.status(503).send("Server error");
+  }
+});
+
+/**
  * Get the logbook information.
  * Query: {
  *  type: 'error' | 'summary' | 'orders'
